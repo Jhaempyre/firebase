487a5cd0438ca003614ad498f9cad9bec18c6ced
jhaemyre <jhaashish.ajha@gmail.com>
2024-02-12 21:23:59 +0530

    SignInwithgoogleIsSucessfool

--- src/auth.js
+++ src/auth.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
-import { getAuth ,createUserWithEmailAndPassword ,signInWithPopup, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getAuth ,signInWithPopup, GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBlQdFoy-Rpg8Ur32zBaQyZAyvWP8HG8EM",
@@ -15,7 +15,27 @@ const firebaseConfig = {
 
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const provider = new GoogleAuthProvider();
   const auth = getAuth(app);
+  auth.languageCode = 'en'
+
+  const Google_Login = document.getElementById('gooh')
+  Google_Login.addEventListener("click",function(){
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+    //window.location.href= "../login.html"
+    console.log(result);
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
+
+  })
 
 
 
